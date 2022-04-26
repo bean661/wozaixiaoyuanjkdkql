af330227241f5d5dc492c87fc5c323c1c8360454
bean661 <85329404+bean661@users.noreply.github.com>
2022-04-26 11:46:19 +0800

    Add files via upload

--- wq.py
+++ wq.py
@@ -169,8 +169,6 @@ class WoZaiXiaoYuanPuncher:
             s = requests.session()
             r = s.post(url, data=post_data, headers=headers)
             res = json.loads(r.text)
-            print(headers)
-            print(str(res)+str(111111))
             if res['code'] == -10:
                 print('jwsession 无效，尝试账号密码晚签')
                 self.status_code = 4
