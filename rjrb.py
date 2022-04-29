c16a8d6cb693a53270213468eb84eb0c44a0d6f9
bean661 <85329404+bean661@users.noreply.github.com>
2022-04-29 22:48:01 +0800

    Add files via upload

--- rjrb.py
+++ rjrb.py
@@ -13,10 +13,13 @@ import json
 
 class pre:
     # 获取answers
-    def get_get_answers(self,i):
-        get_answers = os.getenv("wzxy_rjrb_config" + str(i) + "answers", "null")
+    def get_answers(self,i):
+        get_answers = os.getenv("wzxy_jkdk_config" + str(i) + "answers", "null")
+        get_answers = get_answers.strip('[')
+        get_answers = get_answers.strip(']')
+        get_answers = get_answers.split(',')
         if get_answers == "null":
-            get_answers = '["0","0"]'
+            get_answers = '["0"]'
             print("未获取到用户的"+str(i+1)+"answers,使用默认answers："+str(get_answers))
         else:
             print("获取到用户的"+str(i+1)+"anwsers："+str(get_answers))
